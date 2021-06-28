272a887e61485b802307fbb023e187bc106c8b1c
Redy Apriyadi <redy.apriyadi@gmail.com>
2021-06-28 22:24:00 +0700

    Add log middleware

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"acp9-redy-gigih/config"
+	"acp9-redy-gigih/middlewares"
 	"acp9-redy-gigih/routes"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
+	middlewares.LogMiddleware(e)
 	e.Logger.Fatal(e.Start(":6969"))
 }
---
+++ middlewares/log.go
@@ -0,0 +1,10 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func LogMiddleware(e *echo.Echo) {
+	e.Use(middleware.Logger())
+}
