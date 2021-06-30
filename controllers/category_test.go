4be9e39b71638c53ce10a9a380422b81a2839b46
francino <francinogg@gmail.com>
2021-07-01 05:45:24 +0700

    100% coverage test for Category and Product Controller

--- controllers/category_test.go
+++ controllers/category_test.go
@@ -12,14 +12,21 @@ import (
 	"testing"
 )
 
-func setupEchoDB() *echo.Echo {
-	config.InitDBTest()
-	e := echo.New()
-	return e
+func AddCategoryData() bool {
+	cat := category.Category{Name: "Buku", Description: "Best books on market.", Slug: "buku"}
+	err := config.DB.Create(&cat)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func TestGetCategoriesController(t *testing.T) {
-	e := setupEchoDB()
+	config.InitDBTest()
+	e := echo.New()
+	config.DB.Migrator().DropTable(&category.Category{})
+	config.DB.Migrator().AutoMigrate(&category.Category{})
+	AddCategoryData()
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
@@ -34,8 +41,29 @@ func TestGetCategoriesController(t *testing.T) {
 		json.Unmarshal([]byte(body), &responseCategory)
 
 		assert.Equal(t, responseCategory.Status, true)
-		assert.Equal(t, len(responseCategory.Data), 2)
+		assert.Equal(t, len(responseCategory.Data), 1)
 		assert.Equal(t, responseCategory.Data[0].Name, "Buku")
 	}
+}
 
+func TestFailGetCategoriesController(t *testing.T) {
+	config.InitDBTest()
+	e := echo.New()
+	config.DB.Migrator().DropTable(&category.Category{})
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	c.SetPath("/categories")
+	if assert.NoError(t, GetCategoriesController(c)) {
+		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		body := rec.Body.String()
+		var responseCategory category.CategoryResponse
+		fmt.Println(body)
+		json.Unmarshal([]byte(body), &responseCategory)
+
+		assert.Equal(t, responseCategory.Status, false)
+		assert.Equal(t, len(responseCategory.Data), 0)
+	}
 }
