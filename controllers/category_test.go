5474cd8f9783e82df1d72de1b4323dbe63ae2a25
francino <francinogg@gmail.com>
2021-06-30 15:31:36 +0700

    add test for GetCategoriesController

---
+++ controllers/category_test.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"acp9-redy-gigih/config"
+	"acp9-redy-gigih/models/category"
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func setupEchoDB() *echo.Echo {
+	config.InitDBTest()
+	e := echo.New()
+	return e
+}
+
+func TestGetCategoriesController(t *testing.T) {
+	e := setupEchoDB()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	c.SetPath("/categories")
+	if assert.NoError(t, GetCategoriesController(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		body := rec.Body.String()
+		var responseCategory category.CategoryResponse
+		fmt.Println(body)
+		json.Unmarshal([]byte(body), &responseCategory)
+
+		assert.Equal(t, responseCategory.Status, true)
+		assert.Equal(t, len(responseCategory.Data), 2)
+		assert.Equal(t, responseCategory.Data[0].Name, "Buku")
+	}
+
+}
